12a2012efb8f49f03f624dc3d80fe100a321fd60
theqingyao <2624367622@qq.com>
2024-04-08 20:26:39 +0800

    细节优化

--- apps/群管功能.js
+++ apps/群管功能.js
@@ -1,6 +1,4 @@
 import { Config } from '../components/index.js'
-import dm from '../lib/dm.js'
-const Numreg = '[零一壹二两三四五六七八九十百千万亿\\d]+'
 
 export class example extends plugin {
   constructor () {
@@ -16,8 +14,8 @@ export class example extends plugin {
           permission: 'master'
         },
         {
-          reg: `^#(跑路)?禁言\\s?((\\d+)\\s)?(${Numreg})?(分|分钟|min|m|时|小时|hour|h|天|日|day|d)?$`,
-          fnc: "MuteMember"
+          reg: `^#(跑路)?(假)?全(体|员)(禁言|解禁)$`,
+          fnc: "MuteAll"
         },
       ]
     })
@@ -51,7 +49,7 @@ export class example extends plugin {
     if (!res) return e.reply("少女为你痛哭")
     type ? e.reply(`已设置用户「${name}」为管理员`) : e.reply(`已取消用户「${name}」的管理员权限`)
   }
-    async MuteMember(e) {
+    async MuteAll(e) {
     if (!/跑路/.test(e.msg) && !Config.getConfig('set','sz')['qg']){return false}
     if (!(e.member.is_admin || e.isMaster)){
      this.reply('少女为你痛哭，你好像还没有权限')
@@ -61,29 +59,19 @@ export class example extends plugin {
    this.reply('少女自毁了~')
    return false
    }
-    
-    let qq = e.message.find(item => item.type == "at")?.qq
-    if (!qq || !(/\d{5,}/.test(qq))) return e.reply("少女为你痛哭 \n主人，你好像输入了错误的QQ号")
-    let reg = new RegExp(`^#(跑路)?禁言\\s?((\\d+)\\s)?(${Numreg})?(分|分钟|时|小时|天|日)?$`)
-        let time = dm.translateChinaNum(e.msg.match(reg)[3])
-        let date = e.msg.match(reg)[4]
-        if (date == '分' || date == '分钟') {
-            let bantime = time * 60
-            e.group.muteMember(qq, bantime)
-            this.reply('主人，少女已经将坏人给禁言了')
-        }
-        else if (date == '时' || date == '小时') {
-            let bantime = time * 60 * 60
-            e.group.muteMember(qq, bantime)
-            this.reply('主人，少女已经将坏人给禁言了')
-        }
-        else if (date == '天' || date == '日') {
-            let bantime = time * 60 * 60 * 24
-            e.group.muteMember(qq, bantime)
-        }
-        else 
-        {
-        this.reply('少女为你痛哭 \n主人，你好像输入了错误的时间')
-        }
+   let type;
+    if (/禁言/.test(e.msg)){
+     type = true
+    }else
+    if (/解禁/.test(e.msg)){
+     type = false
+    }
+    if (/假/.test(e.msg)){
+     await e.group._setting({17:1})
+     this.reply('主人，已经为你开启了全体禁言(假)了哦')
+    }
+    let res = await e.group.muteAll(type)
+    if (!res) return e.reply("少女为你痛哭", true)
+    e.reply(`主人，已经为你${type ? "开启" : "关闭"}全体禁言(真)了哦`)
      }
   }
