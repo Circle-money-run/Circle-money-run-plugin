f6654d0351b0ef612a2d48bbd5f8c4cb1362bd2c
theqingyao <2624367622@qq.com>
2024-04-12 20:21:31 +0800

    ✨细节优化🎉

--- apps/随机音频.js
+++ apps/随机音频.js
@@ -1,9 +1,4 @@
-let xxxPatc = process.cwd() + "/plugins/Circle-money-run-plugin/resources/voice/小心心传奇.mp3";
-let cjdlPath = process.cwd() + "/plugins/Circle-money-run-plugin/resources/voice/菜就多练.mp3"
-let mkgcqPath = process.cwd() + "/plugins/Circle-money-run-plugin/resources/voice/模块哥传奇.mp3"
-let yylxUrl = encodeURI(`https://img.kookapp.cn/attachments/2023-09/14/65027028d1a83.mp3`)
 let cPath = process.cwd() + "/plugins/Circle-money-run-plugin/resources/img/1.png"
-let cUrl = encodeURI(`https://img.kookapp.cn/attachments/2024-01/14/65a3b36237503.mp3`)
 
 export class VoiceMessageSender extends plugin {
     constructor() {
@@ -38,28 +33,28 @@ export class VoiceMessageSender extends plugin {
     }
 
     async xxxcq(e) {
-        await this.e.reply(segment.record(xxxPath));
+        await this.e.reply(segment.record('http://api.botqsign.icu/voice/xxx));
         return true
     }
     async cjdl(e) {
-    await this.e.reply(segment.record(cjdlPath))
+    await this.e.reply(segment.record('http://api.botqsign.icu/voice/cai'))
     let msg = "🥬，🍷多🧘‍♂️，📚🙅🏗️🍺🐢🛝，🐜✍️💩🐜✍️，🦀🚀🤢🦀🚀，🫵🔑🫔🫴🐜✍️🔔🧘‍♂️🦀🚀，🫵🤔🫵🆚👶"
     await this.e.reply(msg, true, { at: true })
     return true
     }
     async mkgcq(e) {
-    await this.e.reply(segment.record(mkgcqPath))
+    await this.e.reply(segment.record('http://api.botqsign.icu/voice/mk'))
     let msg = "云端不是最强的，模块才是王道"
     await this.e.reply(msg, true, { at: true })
     return true
     }
     async yylx(e) {
-    await this.e.reply(segment.record(yylxUrl))
+    await this.e.reply(segment.record('http://api.botqsign.icu/voice/lead'))
     return true
     }
     async 114514(e) {
     await this.e.reply(segment.image(cPath), true, { at: true })
-    await this.e.reply(segment.record(cUrl))
+    await this.e.reply(segment.record('http://api.botqsign.icu/voice/114514'))
     return true
     }
 }
