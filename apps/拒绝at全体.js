3ce01fd075865de71208a1b51d2f5ca5c6130ee7
theqingyao <2624367622@qq.com>
2024-06-10 08:57:57 +0800

    ✨细节优化🎉

--- apps/拒绝at全体.js
+++ apps/拒绝at全体.js
@@ -15,18 +15,14 @@ constructor() {
 }
     async at(e){
     if(!Config.getConfig('set','sz')['fatqt']){return false}
-    let jytime = Config.getConfig('set','sz')['bantime']
             		if (e.atall){
 			if (!e.member.is_admin && !e.member.is_owner){
 			if (e.group.is_admin){
 			await e.reply('检测到无权限at全体！开始禁言操作')
 			await e.group.recallMsg(e.message_id)
-			await e.group.muteMember(e.user_id, jytime)
+			await e.group.muteMember(e.user_id, 600)
 			}else{
-				await this.reply(segment.at(this.e.group.info.owner_id))
-				await this.reply(segment.at(this.e.group.info.owner_id))
-				await this.reply([`这个坏人无权限@全体！！！`,segment.at(e.user_id)])
-				await this.reply([`这个坏人无权限@全体！！！`,segment.at(e.user_id)])
+				return true
 			}
 			}
 		}else{
