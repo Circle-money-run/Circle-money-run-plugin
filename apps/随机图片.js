3e949fa92b609b991c40da34d58c3c13df84f7fe
狐璃 <zhilaohu114514@163.com>
2024-07-06 13:48:52 +0000

    新增批量获取
    Signed-off-by: 狐璃 <zhilaohu114514@163.com>

--- apps/随机图片.js
+++ apps/随机图片.js
@@ -7,7 +7,7 @@ export class example extends plugin {
       priority: -10,
       rule: [
         {
-          reg: "^#?随机(猫猫糕|寒暄|心海|小心心|瑜笙|狐狐|猫猫虫|柴郡|七濑|七濑胡桃|胡桃|宵宫|小叶)$",
+          reg: "^#?随机(猫猫糕|寒暄|心海|小心心|瑜笙|狐狐|猫猫虫|柴郡|七濑|七濑胡桃|胡桃|宵宫|小叶)([1-9]|10)?$",
           fnc: 'random',
           log: false
         }
@@ -16,85 +16,78 @@ export class example extends plugin {
   }
 
   async random(e) {
-if (e.msg.includes('随机猫猫糕')) {
-  let buttontext = "猫猫糕"
-  let path = "mmg"
-  return this.send(e,buttontext,path)}
-  
-if (e.msg.includes('随机寒暄')) {
-  let buttontext = "寒暄"
-  let path = "hanxuan"
-  return this.send(e,buttontext,path)}
-  
-if (e.msg.includes('随机心海')) {
-  let buttontext = "心海"
-  let path = "xinhai"
-  return this.send(e,buttontext,path)}
+    const match = e.msg.match(/^#?随机(猫猫糕|寒暄|心海|小心心|瑜笙|狐狐|猫猫虫|柴郡|七濑|七濑胡桃|胡桃|宵宫|小叶)([1-9]|10)?$/);
+    if (!match) return false;
 
-if (e.msg.includes('随机小心心')) {
-  let buttontext = "小心心"
-  let path = "xiaoxinxin"
-  return this.send(e,buttontext,path)}
+    const type = match[1];
+    const count = match[2] ? parseInt(match[2], 10) : 1;
 
-if (e.msg.includes('随机瑜笙')) {
-  let buttontext = "瑜笙"
-  let path = "yusheng"
-  return this.send(e,buttontext,path)}
-
-if (e.msg.includes('随机狐狐')) {
-  let buttontext = "狐狐"
-  let path = "huhu"
-  return this.send(e,buttontext,path)}
-
-if (e.msg.includes('随机猫猫虫')) {
-  let buttontext = "猫猫虫"
-  let path = "mmc"
-  return this.send(e,buttontext,path)}
-
-if (e.msg.includes('随机柴郡')) {
-  let buttontext = "柴郡"
-  let path = "chaijun"
-  return this.send(e,buttontext,path)}
-  
-if (e.msg.includes('随机七濑')) {
-  let buttontext = "七濑胡桃"
-  let path = "qilai"
-  return this.send(e,buttontext,path)}
-  
-if (e.msg.includes('随机胡桃')) {
-  let buttontext = "胡桃"
-  let path = "hutao"
-  return this.send(e,buttontext,path)}
-  
-if (e.msg.includes('随机宵宫')) {
-  let buttontext = "宵宫"
-  let path = "xiaogong"
-  return this.send(e,buttontext,path)}
-
-if (e.msg.includes('小叶')) {
-  let buttontext = "小叶"
-  let path = "xiaoye"
-  return this.send(e,buttontext,path)}
-  }
-  
-  async send(e,buttontext,path) {
-  const randomnumber = Math.random();
-console.log(randomnumber);
-  let url = encodeURI(`http://api.botqsign.icu/${path}?${randomnumber}`)
-  let buttons = [
-        [
-          {
-            text: '再来一张',
-            input: `#随机${buttontext}`,
-            send: true,
-          },
-        ],
-      ];
-      e.reply([
-        segment.image(url),
-        segment.button(...buttons),
-      ]);
-      logger.info(`用户[${logger.green(`${this.e.user_id}`)}] 在群[${logger.red(`${this.e.group_id}`)}] 触发功能 > [${logger.blue('随机图片.js')}]`)
-      return true  
+    if (count > 10) {
+      return e.reply('最多只能10张！');
     }
+
+    const paths = {
+      "猫猫糕": "mmg",
+      "寒暄": "hanxuan",
+      "心海": "xinhai",
+      "小心心": "xiaoxinxin",
+      "瑜笙": "yusheng",
+      "狐狐": "huhu",
+      "猫猫虫": "mmc",
+      "柴郡": "chaijun",
+      "七濑": "qilai",
+      "七濑胡桃": "qilai",
+      "胡桃": "hutao",
+      "宵宫": "xiaogong",
+      "小叶": "xiaoye"
+    };
+
+    let buttontext = type;
+    let path = paths[type];
+    
+    if (count === 1) {
+      return this.send(e, buttontext, path);
+    } else {
+      return this.sendMultiple(e, buttontext, path, count);
+    }
+  }
+
+  async send(e, buttontext, path) {
+    const randomnumber = Math.random();
+    let url = encodeURI(`http://api.botqsign.icu/${path}?${randomnumber}`);
+    let buttons = [
+      [
+        {
+          text: '再来一张',
+          input: `#随机${buttontext}`,
+          send: true,
+        },
+      ],
+    ];
+    e.reply([
+      segment.image(url),
+      segment.button(...buttons),
+    ]);
+    logger.info(`用户[${logger.green(`${this.e.user_id}`)}] 在群[${logger.red(`${this.e.group_id}`)}] 触发功能 > [${logger.blue('随机图片.js')}]`);
+    return true;
+  }
+
+  async sendMultiple(e, buttontext, path, count) {
+    let messages = [];
+    for (let i = 0; i < count; i++) {
+      const randomnumber = Math.random();
+      let url = encodeURI(`http://api.botqsign.icu/${path}?${randomnumber}`);
+      messages.push({
+        message: segment.image(url),
+        nickname: Bot.nickname,
+        user_id: Bot.uin
+      });
+    }
+
+    const forwardMsg = await e.group.makeForwardMsg(messages);
+
+    e.reply(forwardMsg);
+    logger.info(`用户[${logger.green(`${this.e.user_id}`)}] 在群[${logger.red(`${this.e.group_id}`)}] 触发功能 > [${logger.blue('随机图片.js')}]`);
+    return true;
+  }
 }
